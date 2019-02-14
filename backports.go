7707ae5d1261a8929edea7336c8087ca8b520d8d
Ayke van Laethem <aykevanlaethem@gmail.com>
2019-02-14 11:10:53 +0100

    Switch to LLVM 8

--- backports.go
+++ backports.go
@@ -6,33 +6,6 @@ package llvm
 // #include <stdlib.h>
 // #include "backports.h"
 import "C"
-import "unsafe"
-
-// Token type (used by coroutines)
-// https://reviews.llvm.org/D47684
-
-const TokenTypeKind TypeKind = C.LLVMTokenTypeKind
-
-func (c Context) TokenType() (t Type) { t.C = C.LLVMTokenTypeInContext(c.C); return }
-
-// Inline assembly
-// https://reviews.llvm.org/D46437
-
-type InlineAsmDialect C.LLVMInlineAsmDialect
-
-const (
-	InlineAsmDialectATT   InlineAsmDialect = C.LLVMInlineAsmDialectATT
-	InlineAsmDialectIntel InlineAsmDialect = C.LLVMInlineAsmDialectIntel
-)
-
-func InlineAsm(t Type, asmString, constraints string, hasSideEffects, isAlignStack bool, dialect InlineAsmDialect) (rv Value) {
-	casm := C.CString(asmString)
-	defer C.free(unsafe.Pointer(casm))
-	cconstraints := C.CString(constraints)
-	defer C.free(unsafe.Pointer(cconstraints))
-	rv.C = C.LLVMGetInlineAsm(t.C, casm, C.size_t(len(asmString)), cconstraints, C.size_t(len(constraints)), boolToLLVMBool(hasSideEffects), boolToLLVMBool(isAlignStack), C.LLVMInlineAsmDialect(dialect))
-	return
-}
 
 // Coroutine optimization passes
 // https://reviews.llvm.org/D51642 (in progress)
@@ -53,21 +26,6 @@ func (v Value) RemoveFromParentAsInstruction() { C.LLVMInstructionRemoveFromPare
 func (v Value) CalledValue() (rv Value) {
 	rv.C = C.LLVMGetCalledValue(v.C)
 	return
-}
-
-// Indices
-// https://reviews.llvm.org/D53883 (in progress)
-
-func (v Value) Indices() []uint32 {
-	num := C.LLVMGetNumIndices(v.C)
-	indicesPtr := C.LLVMGetIndices(v.C)
-	// https://github.com/golang/go/wiki/cgo#turning-c-arrays-into-go-slices
-	rawIndices := (*[1 << 20]C.uint)(unsafe.Pointer(indicesPtr))[:num:num]
-	indices := make([]uint32, num)
-	for i := range indices {
-		indices[i] = uint32(rawIndices[i])
-	}
-	return indices
 }
 
 // Predicates
