53522ab6713de6a93acdc3f806161e754ec77ea1
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2020-02-11 16:19:32 -0500

    Sync IR changes from LLVM 9.0.1.

--- backports.go
+++ backports.go
@@ -14,27 +14,7 @@ func (pmb PassManagerBuilder) AddCoroutinePassesToExtensionPoints() {
 	C.LLVMPassManagerBuilderAddCoroutinePassesToExtensionPoints_backport(pmb.C)
 }
 
-// Erase instruction
-// https://reviews.llvm.org/D52694 (in progress)
+// Remove instruction
+// https://reviews.llvm.org/D72209 (in progress)
 
-func (v Value) EraseFromParentAsInstruction()  { C.LLVMInstructionEraseFromParent(v.C) }
 func (v Value) RemoveFromParentAsInstruction() { C.LLVMInstructionRemoveFromParent(v.C) }
-
-// Called function from a CallInst
-// https://reviews.llvm.org/D52972 (in progress)
-
-func (v Value) CalledValue() (rv Value) {
-	rv.C = C.LLVMGetCalledValue(v.C)
-	return
-}
-
-// Predicates
-// https://reviews.llvm.org/D53884 (in progress)
-
-func (v Value) IntPredicate() IntPredicate {
-	return IntPredicate(C.LLVMGetICmpPredicate(v.C))
-}
-
-func (v Value) FloatPredicate() FloatPredicate {
-	return FloatPredicate(C.LLVMGetFCmpPredicate(v.C))
-}
