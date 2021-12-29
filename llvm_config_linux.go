1c7d6751d41a818ea61dd2cd18302c1ea5cfd302
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2021-12-29 17:52:54 -0500

    Add a config for LLVM 13

--- llvm_config_linux.go
+++ llvm_config_linux.go
@@ -1,5 +1,5 @@
-//go:build !byollvm && linux && !llvm11
-// +build !byollvm,linux,!llvm11
+//go:build !byollvm && linux && !llvm13 && !llvm11
+// +build !byollvm,linux,!llvm13,!llvm11
 
 package llvm
 
