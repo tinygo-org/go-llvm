75a8a9fe6f741b5284518ba8f52b3b1cd81d0085
Ayke van Laethem <aykevanlaethem@gmail.com>
2023-10-15 01:36:42 +0200

    darwin: remove -lcurses and -lxml2
    Apparently these aren't needed, and at least libcurses isn't available
    on NixOS it seems (it uses libncurses instead).
    
    In summary, this simply improves compatibility with Nix without any
    apparent downsides.

--- llvm_config_darwin_llvm16.go
+++ llvm_config_darwin_llvm16.go
@@ -6,10 +6,10 @@ package llvm
 
 // #cgo amd64 CPPFLAGS: -I/usr/local/opt/llvm@16/include   -D__STDC_CONSTANT_MACROS -D__STDC_FORMAT_MACROS -D__STDC_LIMIT_MACROS
 // #cgo amd64 CXXFLAGS: -std=c++17
-// #cgo amd64 LDFLAGS: -L/usr/local/opt/llvm@16/lib -Wl,-search_paths_first -Wl,-headerpad_max_install_names -lLLVM -lz -lcurses -lm -lxml2
+// #cgo amd64 LDFLAGS: -L/usr/local/opt/llvm@16/lib -Wl,-search_paths_first -Wl,-headerpad_max_install_names -lLLVM -lz -lm
 // #cgo arm64 CPPFLAGS: -I/opt/homebrew/opt/llvm@16/include   -D__STDC_CONSTANT_MACROS -D__STDC_FORMAT_MACROS -D__STDC_LIMIT_MACROS
 // #cgo arm64 CXXFLAGS: -std=c++17
-// #cgo arm64 LDFLAGS: -L/opt/homebrew/opt/llvm@16/lib -Wl,-search_paths_first -Wl,-headerpad_max_install_names -lLLVM -lz -lcurses -lm -lxml2
+// #cgo arm64 LDFLAGS: -L/opt/homebrew/opt/llvm@16/lib -Wl,-search_paths_first -Wl,-headerpad_max_install_names -lLLVM -lz -lm
 import "C"
 
 type run_build_sh int
